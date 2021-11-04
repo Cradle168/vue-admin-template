83400627d697b825bd2b7b345b5e59f1e6f9b30c
biou.jin <374782601@qq.com>
2021-11-04 15:10:12 +0800

    add tinymce custmer.css

--- src/components/TinymceEditor/index.vue
+++ src/components/TinymceEditor/index.vue
@@ -81,6 +81,7 @@ export default {
         plugins: this.plugins,
         toolbar: this.toolbar,
         content_style: 'p {margin: 5px 0;}',
+        content_css: '/static/tinymce/skins/content/writer/customer.css',
         fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
         font_formats:
           '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
