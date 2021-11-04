7b120772bf974c0f41c85057ef0ee19e09ea93d5
biou.jin <374782601@qq.com>
2021-11-04 15:11:33 +0800

    add extended_valid_elements field

--- src/components/TinymceEditor/index.vue
+++ src/components/TinymceEditor/index.vue
@@ -83,6 +83,7 @@ export default {
         content_style: 'p {margin: 5px 0;}',
         content_css: '/static/tinymce/skins/content/writer/customer.css',
         fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
+        extended_valid_elements: 'pre[id|class|title|style|name|codename|data*]',
         font_formats:
           '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
         branding: false,
